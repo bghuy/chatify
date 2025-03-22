3b957218895cc1d9888b2aed4f33f5bf508bf05d
Bạch Gia Huy <bghuy2003@gmail.com>
2025-03-22 18:52:41 +0700

    Hide close icon

--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -56,7 +56,7 @@ export const InitialModal = () => {
 
   return (
     <Dialog open>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black p-0 overflow-hidden [&>button]:hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">Customize your server</DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
